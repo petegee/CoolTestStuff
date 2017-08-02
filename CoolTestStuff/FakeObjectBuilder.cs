d6d2befcd4d6570e6d03cc0cb70b7b47a8d43114
DATACOM-NZ\PeterGla <petergla@datacom.co.nz>
2017-08-03 11:21:54 +1200

    attempting to split SystemUnderTest into two... part one - creating a Fake object builder.

---
+++ CoolTestStuff/FakeObjectBuilder.cs
@@ -0,0 +1,83 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CoolTestStuff
+{
+    public class FakeObjectBuilder<TSut> where TSut : class
+    {
+        private List<KeyValuePair<string, object>> specifiedDependencies;
+
+        public FakeObjectBuilder()
+        {
+            specifiedDependencies = new List<KeyValuePair<string, object>>();
+        }
+
+        public FakeObjectBuilder(List<KeyValuePair<string, object>> specificInstances)
+        {
+            specifiedDependencies = specificInstances;
+        }
+
+        private object[] GetMostSpecialisedConstructorParameterValues()
+        {
+            var constructorValues = new List<object>();
+            foreach (var param in GetMostSpecialisedConstructor().GetParameters())
+            {
+                var specifiedDependency = GetSpecifiedInstance(param);
+                if (specifiedDependency != null)
+                {
+                    constructorValues.Add(specifiedDependency.Value);
+                    continue;
+                }
+
+                if (CanBeMocked(param.ParameterType))
+                {
+                    var mockInstance = CreateMock(param.ParameterType);
+                    //systemUnderTestMocks.Add(
+                    //    new RegisteredMock
+                    //    {
+                    //        Mock = mockInstance,
+                    //        TypeThatHasBeenMocked = param.ParameterType,
+                    //        NameOfMockInstance = param.Name
+                    //    });
+
+                    constructorValues.Add(mockInstance.Object);
+                    continue;
+                }
+
+                constructorValues.Add(GetDefault(param.ParameterType));
+            }
+
+            return constructorValues.ToArray();
+        }
+
+        private KeyValuePair<string, object>? GetSpecifiedInstance(ParameterInfo paramInfo)
+        {
+            return specifiedDependencies
+                .FirstOrDefault(
+                    o =>
+                        paramInfo.ParameterType.IsInstanceOfType(o.Value) &&
+                        paramInfo.Name == (o.Key ?? paramInfo.Name));
+        }
+
+        private static bool CanBeMocked(Type dependencyType)
+            => dependencyType.IsClass || dependencyType.IsInterface;
+
+        private static ConstructorInfo GetMostSpecialisedConstructor()
+        {
+            var allCtors = typeof(TSut).GetConstructors();
+            var maxParams = allCtors.Max(ctor => ctor.GetParameters().Length);
+            return allCtors.Single(ctor => ctor.GetParameters().Length == maxParams);
+        }
+
+        private static Mock CreateMock(Type dependencyType)
+            => (Mock)typeof(Mock<>).MakeGenericType(dependencyType)
+                    .GetConstructor(new Type[0])
+                    ?.Invoke(new object[0]);
+
+        private static object GetDefault(Type type)
+            => type.IsValueType ? Activator.CreateInstance(type) : null;
+    }
+}
---
+++ CoolTestStuff/LocalAddin.cs
@@ -0,0 +1,9 @@
+using NUnit.Core.Extensibility;
+
+namespace CoolTestStuff
+{
+    [NUnitAddin]
+    public class LocalAddin : Ploeh.AutoFixture.NUnit2.Addins.Addin
+    {   
+    }
+}
